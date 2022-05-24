0f44e2a840aca5109fb7fb0e98f4e3c50539e970
felipe.miranda.pires@escola.pr.gov.br <felipe.miranda.pires@escola.pr.gov.br>
2022-05-24 17:15:50 -0300

    style inicio

--- teste.js
+++ teste.js
@@ -2,3 +2,9 @@ $('a.link').click(function(event) {
 	event.preventDefault();
 	$('div.content').html($('div', this).html());
 });
+
+function tester() {
+	alert("mensagem");
+	window.location.href = "noticia.html";
+	// ou uma variante com o mesmo efeito
+}
